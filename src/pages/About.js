aa8f299331ccf02460eaa8684574dd5f6409f075
nunegabriel <nune.gabriel.gn@gmail.com>
2022-11-12 07:56:25 +0000

    Add styles and Structure

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+  <div className="about__content">
+    <p className="about">
+      React Todo App
+      This is a simple app that adds a list of elements.
+      You can add, edit and delete any item added
+    </p>
+  </div>
+);
+export default About;
